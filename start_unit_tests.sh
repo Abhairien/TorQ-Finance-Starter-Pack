f06f8f13c658f5fe1a82a900d6aa1e37a56ea311
AndrewClugston <andrew.clugston@aquaq.co.uk>
2018-03-29 12:01:52 +0100

    Added sym backup test

--- start_unit_tests.sh
+++ start_unit_tests.sh
@@ -5,4 +5,4 @@ export KDBSTACKID="-stackid ${KDBBASEPORT}"
 
 #Execute Unit tests
 echo 'Executing Unit Tests...'
-nohup q torq.q -procname unittests1 -proctype unittests -load tests/k4unit.q -test tests/ -debug
+q torq.q -procname unittests1 -proctype unittests -load tests/k4unit.q -test tests/ -debug
